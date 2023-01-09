f0543cdb369311aba62702ad90a3b0d33cf45394
Andrea Hergert <ahergert24@hotmail.com>
2023-01-09 11:26:01 -0800

    added about me info to about me page

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -5,7 +5,16 @@ export default function About() {
     <div>
       <h1>About Me</h1>
       <p>
-        About me.
+        UW Coding Bootcamp Student who's learning full-stack web development. Former Librarian.  As a full-stack web developer, I am a driven and self-motivated individual with a passion for technology and a desire to make a positive impact through my work. I have a strong foundation in programming languages and a proven ability to learn and adapt quickly in a fast-paced environment. With a focus on delivering high-quality, reliable solutions, I am committed to constantly improving my skills and staying up-to-date with the latest technologies. Whether working independently or as part of a team, I am always striving to contribute to the success of the project and the company.
+      </p>
+      <p>
+        I have fourteen years of library experience, with six of those years working for Jefferson County Library as a Librarian I in Information Services/Cataloging. As a Librarian I in Information Services/Cataloging at Jefferson County Library I assisted patrons and demonstrated the use of reference resources at the information desk, helped patrons with item requests, and helped patrons with the public computers as well as with their own tech devices. My other tasks include cataloging, collection development, and library programming.
+      </p>
+      <p>
+        My previous employment was for nearly two years as a Library Assistant in Cataloging at Whitman College. My main task there was providing original and copy cataloging of books, DVDs, and electronic resources. Another responsibility I had was supervising student workers which included monitoring their work, approving schedules, checking timesheets, and training them to catalog government documents.
+      </p>
+      <p>
+        My first library job was for six years as a Library Assistant at the Whitman County Library. My main duties as a Library Assistant were helping library patrons at the reference and circulation desk, coordinating a home library delivery service at two senior living facilities, purchasing and managing the collection for popular movies, music, and eBooks. Also, I was teaching public programs about eBooks, social media, and internet safety, assisting with interlibrary loan, and cataloging movies, music, and large-print books.
       </p>
     </div>
   );
