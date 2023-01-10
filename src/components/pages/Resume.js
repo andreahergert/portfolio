dae6e9593bd3e2ae154dc7685d927fbcd5db2e2f
Andrea Hergert <ahergert24@hotmail.com>
2023-01-10 13:17:03 -0800

    added draft components for resume will update format

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -5,7 +5,54 @@ export default function Resume() {
     <div>
       <h2>Resume</h2>
       <p>
-        My Resume.
+        Download Full Resume
+      </p>
+      <p>
+      SUMMARY
+As a full-stack web developer with a web development certificate from the University of Washington Professional & Continuing Education, I am a driven and self-motivated individual with a passion for technology and a desire to make a positive impact through my work. I have a strong foundation in programming languages and a proven ability to learn and adapt quickly in a fast-paced environment. With a focus on delivering high-quality, reliable solutions, I am committed to constantly improving my skills and staying up-to-date with the latest technologies. Whether working independently or as part of a team, I am always striving to contribute to the success of the project and the company.
+
+      </p>
+      <p>
+      TECHNICAL SKILLS
+Front-end: HTML, CSS, Javascript, JQuery, Bootstrap, React, Responsive Design
+Back-end: MySQL, Sequelize, MongoDB, Mongoose, Express, Node, APIs
+      </p>
+      <p>
+      WORK EXPERIENCE 
+Librarian I – Information Services / Cataloging
+Jefferson County Library, Port Hadlock, WA (5/2016 – 7/2022)
+●	Copy and original cataloging of books, DVDs, CDs, and electronic resources in the Polaris Database
+●	Select and manage the collection for nonfiction, audiobooks, and eAudiobooks
+●	Develop and deliver various library programs and services including technology classes and one-on-one tech help
+●	Assist patrons and demonstrate the use of reference resources at the Info Desk
+
+Library Assistant – Cataloging
+Whitman College, Walla Walla, WA (6/2014 - 4/2016)
+●	Copy and original cataloging of books, DVDs, and electronic resources in the III and Alma Database
+●	Assist in the hiring, training, scheduling, and supervision of student workers
+●	Verify and assign Library of Congress call numbers and subject headings to records
+●	Weeding the government documents collection
+
+Library Assistant
+Whitman County Library, Colfax, WA (5/2008 - 5/2014)
+●	Copy and original cataloging of popular movies, music CDs, and large print books in the Horizon Database
+●	Develop and deliver various library programs and services including technology classes
+●	Process, borrow, and lend interlibrary loans in WorldShare
+●	Assist patrons at the reference and circulation desk
+●	Organize and deliver home library services at local senior living facilities
+●	Select and manage the collection for popular movies, music, and eBooks
+      </p>
+      <p>
+      EDUCATION
+Coding Bootcamp Certificate of Completion					2/2023
+University of Washington Professional & Continuing Education, Seattle, WA
+
+Master of Library and Information Science					5/2014
+San José State University, San José, CA	
+					
+Bachelor of Arts in Literature							3/2008
+The Evergreen State College, Olympia, WA
+
       </p>
     </div>
   );
