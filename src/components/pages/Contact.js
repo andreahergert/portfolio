3b77a7890e02f114892a98bd4fff1f17fe2d687d
Andrea Hergert <ahergert24@hotmail.com>
2023-01-10 16:31:37 -0800

    added helpers for email validation added contact form

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,12 +1,81 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-export default function Contact() {
+import { validateEmail } from '../../utils/helpers';
+
+function Form() {
+  const [email, setEmail] = useState('');
+  const [Name, setName] = useState('');
+  const [message, setMessage] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const handleInputChange = (e) => {
+    const { target } = e;
+    const inputType = target.name;
+    const inputValue = target.value;
+
+    if (inputType === 'email') {
+      setEmail(inputValue);
+    } else if (inputType === 'Name') {
+      setName(inputValue);
+    } else {
+      setMessage(inputValue);
+    }
+  };
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+
+
+    if (!validateEmail(email)) {
+      setErrorMessage('Email is invalid');
+      return;
+    }
+
+    setName('');
+    setMessage('');
+    setEmail('');
+  };
+
   return (
     <div>
       <h2>Contact</h2>
-      <p>
-        Contact me.
-      </p>
+      <form className="form">
+        <div>
+        <input
+          value={email}
+          name="email"
+          onChange={handleInputChange}
+          type="email"
+          placeholder="email"
+        />
+        </div>
+        <div>
+        <input
+          value={Name}
+          name="Name"
+          onChange={handleInputChange}
+          type="text"
+          placeholder="name"
+        />
+        </div>
+        <input
+          value={message}
+          name="message"
+          onChange={handleInputChange}
+          type="message"
+          placeholder="message"
+        />
+        <div>
+        <button type="button" onClick={handleFormSubmit}>Submit</button>
+        </div>
+      </form>
+      {errorMessage && (
+        <div>
+          <p className="error-text">{errorMessage}</p>
+        </div>
+      )}
     </div>
   );
 }
+
+export default Form;
