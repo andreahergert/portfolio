84ba02b964fe60659c5d504e8a23be645eedc3b1
Andrea Hergert <ahergert24@hotmail.com>
2023-01-11 15:12:06 -0800

    added different links and style

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,10 +5,10 @@ function Footer() {
     <footer className="text-center">
       <div className="container p-4">
         <section className="mb-4">
-          <a className="btn btn-primary btn-floating m-1" href="https://github.com/andreahergert" target="blank" role="button">Github<i className="fab fa-github"></i></a>
-          <a className="btn btn-primary btn-floating m-1" href="https://www.linkedin.com/in/andreahergert/" target="blank" role="button">LinkedIn<i className="fab fa-linkedin-in"></i></a>
-          <a className="btn btn-primary btn-floating m-1" href="https://www.goodreads.com/andreahergert" target="blank" role="button">Goodreads<i className="fab fa-linkedin-in"></i></a>
-          <a className="btn btn-primary btn-floating m-1" href="https://post.news/@andreahergert" target="blank" role="button">Post<i className="fab fa-linkedin-in"></i></a>
+          <a className="btn btn-primary btn-floating m-1" href="https://github.com/andreahergert" target="blank" role="button">Github</a>
+          <a className="btn btn-primary btn-floating m-1" href="https://www.linkedin.com/in/andreahergert/" target="blank" role="button">LinkedIn</a>
+          <a className="btn btn-primary btn-floating m-1" href="https://twitter.com/AndreaHergert" target="blank" role="button">Twitter</a>
+          <a className="btn btn-primary btn-floating m-1" href="https://post.news/@andreahergert" target="blank" role="button">Post</a>
         </section>
       </div>
     </footer>
