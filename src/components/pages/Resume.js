89044260e8768e945dd4dd39ff08708ffa260677
Andrea Hergert <ahergert24@hotmail.com>
2023-01-11 14:39:22 -0800

    took out repeated summary

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -16,19 +16,6 @@ export default function Resume() {
           Download Full Resume
         </a>
       </p>
-      <h3>Summary</h3>
-      <p>
-        As a full-stack web developer with a web development certificate from
-        the University of Washington Professional & Continuing Education, I am a
-        driven and self-motivated individual with a passion for technology and a
-        desire to make a positive impact through my work. I have a strong
-        foundation in programming languages and a proven ability to learn and
-        adapt quickly in a fast-paced environment. With a focus on delivering
-        high-quality, reliable solutions, I am committed to constantly improving
-        my skills and staying up-to-date with the latest technologies. Whether
-        working independently or as part of a team, I am always striving to
-        contribute to the success of the project and the company.
-      </p>
       <h3>Technical Skills</h3>
       <p>
         <h4 >Front-End</h4>
