023b352ad1844856abca68e2518b914cabd87e83
Andrea Hergert <ahergert24@hotmail.com>
2023-01-12 08:33:12 -0800

    took out link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,7 +7,6 @@ function Footer() {
         <section className="mb-4">
           <a className="btn btn-primary btn-floating m-1" href="https://github.com/andreahergert" target="blank" role="button">Github</a>
           <a className="btn btn-primary btn-floating m-1" href="https://www.linkedin.com/in/andreahergert/" target="blank" role="button">LinkedIn</a>
-          <a className="btn btn-primary btn-floating m-1" href="https://twitter.com/AndreaHergert" target="blank" role="button">Twitter</a>
           <a className="btn btn-primary btn-floating m-1" href="https://post.news/@andreahergert" target="blank" role="button">Post</a>
         </section>
       </div>
