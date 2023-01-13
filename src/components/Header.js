1cb3fa4c11ad859fd85ed5282633c57b4947b06c
Andrea Hergert <ahergert24@hotmail.com>
2023-01-12 22:23:10 -0800

    navbar formatting

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,60 +1,72 @@
-import React from 'react';
+import React from "react";
+import '../style/header.css';
 
 const styles = {
   navbar: {
-    backgroundColor: '#58C2C7',
+    backgroundColor: "#58C2C7",
   },
   navbarA: {
-    color: '#0000FF',
-    fontSize: '19px',
+    color: "#0000FF",
+    fontSize: "19px",
   },
-  h1:{
-    fontSize: '50px',
+  h1: {
+    fontSize: "50px",
+  },
+  padding: {
+    padding: "8px",
   }
 };
 
 function Header({ currentPage, handlePageChange }) {
   return (
     <nav style={styles.navbar}>
-      <header className='nav justify-content-center'><h1 style={styles.h1}>Andrea Hergert</h1></header>
-    <ul style={styles.navbarA} className='nav justify-content-center'>
-      <li className="nav-item">
-        <a
-          href="#about"
-          onClick={() => handlePageChange('About')}
-          className={currentPage === 'About' ? 'nav-link-active' : 'nav-link'}
-        >
-          About
-        </a>
-      </li>
-      <li className="nav-item">
-        <a
-          href="#portfolio"
-          onClick={() => handlePageChange('Portfolio')}
-          className={currentPage === 'Portfolio' ? 'nav-link-active' : 'nav-link'}
-        >
-          Portfolio
-        </a>
-      </li>
-      <li className="nav-item">
-        <a
-          href="#contact"
-          onClick={() => handlePageChange('Contact')}
-          className={currentPage === 'Contact' ? 'nav-link-active' : 'nav-link'}
-        >
-          Contact
-        </a>
-      </li>
-      <li className="nav-item">
-        <a
-          href="#resume"
-          onClick={() => handlePageChange('Resume')}
-          className={currentPage === 'Resume' ? 'nav-link-active' : 'nav-link'}
-        >
-          Resume
-        </a>
-      </li>
-    </ul>
+      <header className="nav justify-content-center">
+        <h1 style={styles.h1}>Andrea Hergert</h1>
+      </header>
+      <ul style={styles.navbarA} className="nav justify-content-center">
+        <li style={styles.padding} className="nav-item">
+          <a
+            href="#about"
+            onClick={() => handlePageChange("About")}
+            className={currentPage === "About" ? "nav-link-active" : "nav-link"}
+          >
+            About
+          </a>
+        </li>
+        <li style={styles.padding}  className="nav-item">
+          <a
+            href="#portfolio"
+            onClick={() => handlePageChange("Portfolio")}
+            className={
+              currentPage === "Portfolio" ? "nav-link-active" : "nav-link"
+            }
+          >
+            Portfolio
+          </a>
+        </li>
+        <li style={styles.padding} className="nav-item">
+          <a
+            href="#contact"
+            onClick={() => handlePageChange("Contact")}
+            className={
+              currentPage === "Contact" ? "nav-link-active" : "nav-link"
+            }
+          >
+            Contact
+          </a>
+        </li>
+        <li style={styles.padding} className="nav-item">
+          <a
+            href="#resume"
+            onClick={() => handlePageChange("Resume")}
+            className={
+              currentPage === "Resume" ? "nav-link-active" : "nav-link"
+            }
+          >
+            Resume
+          </a>
+        </li>
+      </ul>
     </nav>
   );
 }
